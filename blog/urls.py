8eab1de8c67fcd7b039115e66b0f396564f7210c
Vinícius Pizzi <pizzivinicius@gmail.com>
2026-01-10 13:34:34 -0300

    Adicionando template unico para cada post e refatorando algumas coisas

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from blog import views as blog_views
 
-
+# blog/
 urlpatterns = [
     path('', blog_views.blog, name='blog.home'),
+    path('post/<int:post_id>/', blog_views.post, name='blog.post'),
     path('comentarios/', blog_views.comentarios, name='blog.comentarios'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,12 +16,33 @@ def blog(request):
         'blog/index.html',
         context=context,
     )
+    
+def post(request, post_id):
+    post_data = None
+    for post in data.posts:
+        if post['id'] == int(post_id):
+            post_data = post
+            break
+    
+    context = {
+        'text' : 'ESTAMOS NO POST',
+        'title' : f"Post {post_id}",
+        'post' : post_data,
+    }
+    
+    return render(
+        request,
+        'partials/post.html',
+        context=context,
+    )   
+    
 
 def comentarios(request):
     
     context = {
         'text' : 'ESTAMOS NO COMENTARIOS',
-        'title' : 'comentarios'
+        'title' : 'comentarios',
+        'coments' : data.coments,
     }
     
     return render(
